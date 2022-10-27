1cce570906b259579786dd79632a72a381f69a85
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-10-27 11:44:53 -0600

    Set Up Background Grid
    Also started work on the FileObject position data

--- FileObjects/FileObject.gd
+++ FileObjects/FileObject.gd
@@ -2,3 +2,11 @@ extends Window
 
 class_name FileObject
 
+func _ready():
+	get_viewport().size_changed.connect(viewport_resized);
+
+func update_position():
+	pass;
+
+func viewport_resized():
+	pass;
