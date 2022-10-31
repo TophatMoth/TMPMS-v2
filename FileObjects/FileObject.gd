2c552e3ee1386362b2a1375114166999108fc645
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-10-31 16:09:38 -0600

    Update to Position Code
    - Now updates global_position when a window moves
    - global_position is now a Vector2i

--- FileObjects/FileObject.gd
+++ FileObjects/FileObject.gd
@@ -1,25 +1,44 @@
 extends Window
-
 class_name FileObject
 
-@export var global_position : Vector2 = Vector2(0.0, 0.0):
+@export var global_position : Vector2i = Vector2(0, 0):
 	set(value):
 		global_position = value;
 		if get_tree() != null:
-			update_position();
+			update_local_position();
+		else:
+			last_local_position = position;
+
+var last_local_position : Vector2i = Vector2i.ZERO;
 
 func _ready():
 	get_tree().root.size_changed.connect(viewport_resized);
 	await get_tree().process_frame;
-	update_position();
+	update_local_position();
 
-func update_position():
+func update_local_position():
 	var camera : Camera2D = get_tree().root.get_camera_2d();
-	if camera == null:
-		return;
-	# Might be wrong by a pixel if screen size is odd, but it doen't matter
-	var top_left : Vector2i = Vector2i(camera.position) - (get_tree().root.size / 2);
-	position = Vector2i(global_position) - top_left;
+	var top_left : Vector2i = Vector2i.ZERO;
+	if camera != null:
+		# Might be wrong by a pixel if screen size is odd, but it doen't matter
+		top_left = Vector2i(camera.position) - (get_tree().root.size / 2);
+	position = global_position - top_left;
+	last_local_position = position;
+
+func update_global_position():
+	var camera : Camera2D = get_tree().root.get_camera_2d();
+	var top_left : Vector2i = Vector2i.ZERO;
+	if camera != null:
+		# Might be wrong by a pixel if screen size is odd, but it doen't matter
+		top_left = Vector2i(camera.position) - (get_tree().root.size / 2);
+	global_position = position + top_left;
+	last_local_position = position;
 
 func viewport_resized():
-	update_position();
+	update_local_position();
+
+func _process(_delta):
+	if last_local_position != position:
+		print(String(name) + " moved");
+		update_global_position();
+		last_local_position = position;
