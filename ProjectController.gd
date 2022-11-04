1798be825bbe790800240ee81a2855cfc7988ab5
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-11-04 00:10:41 -0600

    Update ProjectController.gd
    - Added a placeholder function for saving data
    - Made the save and close buttons use this function
    - Made the close button close the program after saving

--- ProjectController.gd
+++ ProjectController.gd
@@ -19,9 +19,10 @@ func _on_ui_button_pressed(button_id : int):
 			center_cam();
 			center_files();
 		ProjectUI.SAVE:
-			pass;
+			save_data();
 		ProjectUI.CLOSE:
-			pass;
+			save_data();
+			get_tree().quit();
 		ProjectUI.ADD_TEXTBOX:
 			spawn_file_object(textbox_scene);
 		ProjectUI.ADD_FOLDER:
@@ -43,3 +44,6 @@ func spawn_file_object(file_object_scene : PackedScene):
 	$FileObjects.add_child(new_fo);
 	if new_fo is FileObject:
 		new_fo.global_position = $Camera2D.position;
+
+func save_data():
+	print("Data would be saved if this function was implemented");
